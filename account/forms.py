1a7447126c77c6469e5d64a92b4289e406a6c5d4
Kramar_17 <vetal.kramarov@gmail.com>
2024-06-07 12:44:13 +0300

    fix labels

--- account/forms.py
+++ account/forms.py
@@ -1,3 +1,9 @@
+import re
+from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+
 class RegisterForm(forms.ModelForm):
     password1 = forms.CharField(
         widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Пароль'}),
@@ -43,3 +49,40 @@ class RegisterForm(forms.ModelForm):
                 'required': 'Обов`язкове поле',
             }
         }
+
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+
+        username = username.replace(' ', '')
+        if not re.match(r'^(\+?380|0)\d{9}$', username):
+            raise ValidationError('Невірний формат номеру телефону.')
+        if username.startswith('+380'):
+            username = '0' + username[4:]
+        elif username.startswith('380'):
+            username = '0' + username[3:]
+        return username
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError('Паролі не співпадають')
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Номер телефону'})
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Пароль'})
+    )
+
