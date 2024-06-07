fb2e77287fb172733b143ea5a9fcdc303f629f84
Kramar_17 <vetal.kramarov@gmail.com>
2024-06-07 12:41:50 +0300

    add labels to login form

--- account/forms.py
+++ account/forms.py
@@ -1,17 +1,11 @@
-import re
-from django import forms
-from django.contrib.auth.models import User
-from django.core.exceptions import ValidationError
-
-
 class RegisterForm(forms.ModelForm):
     password1 = forms.CharField(
         widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Пароль'}),
-        label='Password'
+        label='Пароль'
     )
     password2 = forms.CharField(
         widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Підтвердження паролю'}),
-        label='Confirm Password'
+        label='Підтвердження паролю'
     )
 
     class Meta:
@@ -23,7 +17,12 @@ class RegisterForm(forms.ModelForm):
             'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Мобільний номер'}),
             'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),
         }
-
+        labels = {
+            'first_name': 'Ім`я',
+            'last_name': 'Прізвище',
+            'username': 'Мобільний номер',
+            'email': 'Електронна пошта',
+        }
         help_texts = {
             'first_name': 'Обов`язкове поле',
             'last_name': 'Обов`язкове поле',
@@ -44,39 +43,3 @@ class RegisterForm(forms.ModelForm):
                 'required': 'Обов`язкове поле',
             }
         }
-
-    def clean_username(self):
-        username = self.cleaned_data['username']
-
-        username = username.replace(' ', '')
-        if not re.match(r'^(\+?380|0)\d{9}$', username):
-            raise ValidationError('Невірний формат номеру телефону.')
-        if username.startswith('+380'):
-            username = '0' + username[4:]
-        elif username.startswith('380'):
-            username = '0' + username[3:]
-        return username
-
-    def clean_password2(self):
-        password1 = self.cleaned_data.get('password1')
-        password2 = self.cleaned_data.get('password2')
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError('Паролі не співпадають')
-        return password2
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.set_password(self.cleaned_data['password1'])
-        if commit:
-            user.save()
-        return user
-
-
-class LoginForm(forms.Form):
-    username = forms.CharField(
-        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Номер телефону'})
-    )
-    password = forms.CharField(
-        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Пароль'})
-    )
-
